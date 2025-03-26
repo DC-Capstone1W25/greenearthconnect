4c4e0698e888b4235519ba73168e7431e992ed7e
Thedyson <90479286+ThedysonLuzon@users.noreply.github.com>
2025-03-26 18:25:50 -0400

    edited client.js

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -4,11 +4,10 @@ import { setContext } from '@apollo/client/link/context';
 
 const isDev = process.env.NODE_ENV !== 'production';
 const baseURL = process.env.REACT_APP_BACKEND_URL || (isDev ? 'http://localhost:5000' : '');
-const graphqlUri = `${baseURL}/graphql`;
+const httpLink = createHttpLink({ uri: `${baseUrl}/graphql` });
 
 console.log('Using GraphQL endpoint:', graphqlUri);
 
-const httpLink = createHttpLink({ uri: graphqlUri });
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('token');
